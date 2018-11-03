28859536daf505c295a5cce08766f2327601dfd2
rrapstine <richard.rapstine@gmail.com>
2018-11-03 00:42:04 -0500

    Hello world and component structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Import required libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create the 'App' component
+const App = () => {
+    return <div>Hello World!</div>;
+}
+
+// Render 'App' component to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
