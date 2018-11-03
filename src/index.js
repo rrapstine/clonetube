244e7e36aa1581eca7b999d14c87f30146d1992e
rrapstine <richard.rapstine@gmail.com>
2018-11-03 01:00:45 -0500

    Add youtube-api-search package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// YouTube API key
+const API_KEY = 'AIzaSyAcY4Qsi-NbckbRgQN756W-zTrsXP1p5Hw';
+
 // Create the 'App' component
 const App = () => {
     return <div>Hello World!</div>;
