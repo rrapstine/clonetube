7631ce80802eded1a3270e7799c84aa9aa86dd8d
rrapstine <richard.rapstine@gmail.com>
2018-11-03 01:12:20 -0500

    Creat search bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+// Import React library
+import React from 'react';
+
+// Create the search bar
+const SearchBar = () => {
+    return <input />;
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Import components
+import SearchBar from './components/search_bar';
+
 // YouTube API key
 const API_KEY = 'AIzaSyAcY4Qsi-NbckbRgQN756W-zTrsXP1p5Hw';
 
 // Create the 'App' component
 const App = () => {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Render 'App' component to the DOM
